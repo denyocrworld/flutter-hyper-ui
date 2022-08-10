9ab476499fcde13484e129280cff137317de3ffc
Deny <flutterlabz@gmail.com>
2022-08-10 21:36:37 +0700

    >

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter_hyper_extension_ui/core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hyper_extension_ui/shared/util/get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   return runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    navigatorKey: Get.navigatorKey,
     theme: defaultTheme,
     home: Scaffold(
       appBar: AppBar(
