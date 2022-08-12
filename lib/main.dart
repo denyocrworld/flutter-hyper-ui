7623b04247cf0d11dbef6e8c2caf4c87ae3fa908
Deny <denyocr.world@gmail.com>
2022-08-12 22:21:48 +0700

    >

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_hyper_ui/core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hyper_ui/demo.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,6 +7,40 @@ void main() async {
   return runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: defaultTheme,
-    home: const FlutterHyperUiKitDemoView(),
+    home: const HelloWorldView(),
   ));
+}
+
+class HelloWorldView extends StatelessWidget {
+  const HelloWorldView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+        actions: [
+          InkWell(
+            onTap: () {},
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(Icons.tune),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(Icons.notifications),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+        ),
+      ),
+    );
+  }
 }
