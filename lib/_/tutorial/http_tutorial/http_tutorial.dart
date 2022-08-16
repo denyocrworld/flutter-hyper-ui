b4936122b36383c5d71b45051e8e58f122b71904
Deny <denyocr.world@gmail.com>
2022-08-16 07:30:12 +0700

    .

--- lib/_/tutorial/http_tutorial/http_tutorial.dart
+++ lib/_/tutorial/http_tutorial/http_tutorial.dart
@@ -5,6 +5,6 @@ class HttpTutorialView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold();
   }
 }
---
+++ lib/setup.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Future initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+}
