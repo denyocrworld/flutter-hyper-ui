7860236b6bfcae26b550658e8071abcbe86b333b
Deny <denyocr.world@gmail.com>
2022-08-17 16:49:49 +0700

    .

--- lib/_/credit/credit.dart
+++
@@ -1,9 +0,0 @@
-class ZffTrXSpecialCredit {
-  /*
-  https://capekngoding.com
-  https://www.youtube.com/c/capekngoding
-
-  Flutter Hyper Extension:
-  https://marketplace.visualstudio.com/items?itemName=denyocr.flutter-hyper-extension
-  */
-}
