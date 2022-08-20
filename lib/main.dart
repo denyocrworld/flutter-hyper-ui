2c7da080164fade5b68a255d036587b74044f6d1
Deny <denyocr.world@gmail.com>
2022-08-20 09:15:58 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:flutter_hyper_ui/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hyper_ui/setup.dart';
 
 void main() async {
   await initialize();
-  return runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: defaultTheme,
-    home: const Scaffold(),
-  ));
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    ),
+  );
 }
