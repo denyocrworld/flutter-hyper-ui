667a51e2bf9608d7121cc852cf9eac4108f774af
Deny <denyocr.world@gmail.com>
2022-08-22 05:36:25 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hyper_ui/core.dart';
 import 'package:flutter_hyper_ui/setup.dart';
@@ -5,11 +6,20 @@ import 'package:get/get.dart';
 
 void main() async {
   await initialize();
+
+  //ganti dengan halaman awal yang ingin dibuka ya
+  Widget mainView = Container();
+
+  if (FirebaseAuth.instance.currentUser != null) {
+    //? jika user sedang dalam keadaan login
+    mainView = Container();
+  }
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: defaultTheme,
-      home: const Scaffold(),
+      home: mainView,
     ),
   );
 }
--- lib/setup.dart
+++ lib/setup.dart
@@ -1,5 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hyper_ui/firebase_options.dart';
+import 'package:flutter_hyper_ui/shared/util/firebase/firebase_auth_ext.dart';
 
 Future initialize() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await FirebaseAuth.instance.wait();
 }
