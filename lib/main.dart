b84463607cd02bd03ac40c78d0415e73c4c118da
Deny <denyocr.world@gmail.com>
2022-08-22 10:24:50 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,21 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hyper_ui/core.dart';
 import 'package:flutter_hyper_ui/setup.dart';
 import 'package:get/get.dart';
-
 void main() async {
-  await initialize();
+    await initialize();
 
-  //ganti dengan halaman awal yang ingin dibuka ya
-  Widget mainView = Container();
-
-  if (FirebaseAuth.instance.currentUser != null) {
-    //? jika user sedang dalam keadaan login
+    Widget mainView = Container();
+    if (FirebaseAuth.instance.currentUser != null) {
     mainView = Container();
-  }
+    }
 
-  runApp(
+    runApp(
     GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: defaultTheme,
-      home: mainView,
+        debugShowCheckedModeBanner: false,
+        theme: defaultTheme,
+        home: mainView,
     ),
   );
 }
