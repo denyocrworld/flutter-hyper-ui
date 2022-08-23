849a39dfa67037d1a3c07c99be67b84b99896770
Deny <denyocr.world@gmail.com>
2022-08-23 20:11:50 +0700

    .

--- zcli.dart
+++ zcli.dart
@@ -3,8 +3,31 @@ import 'dart:io';
 
 String command = "";
 String value = "";
+String packageName = "";
+getPackageName() async {
+  var pubSpecFile = File("./pubspec.yaml");
+  var content = await pubSpecFile.readAsString(
+    encoding: utf8,
+  );
+
+  var lines =
+      content.contains("\r\n") ? content.split("\r\n") : content.split("\n");
+
+  var selectedLines =
+      lines.where((line) => line.startsWith("name: ")).toList()[0];
+
+  packageName = selectedLines.split("name: ")[1];
+  packageName = packageName.replaceAll("\r\n", "");
+  packageName = packageName.replaceAll("\n", "");
+  packageName = packageName.replaceAll(RegExp(r"/^\s\n+|\s\n+$/g"), "");
+  packageName = packageName;
+
+  packageName.split("\n")[0];
+}
 
 void main(args) async {
+  await getPackageName();
+
   command = args.isNotEmpty ? args[0] : "";
   value = args.length > 1 ? args[1] : "";
 
@@ -225,28 +248,6 @@ removeAllUnusedPackages() async {
 
   await removeUnusedPackages();
   return;
-}
-
-String packageName = "";
-void getPackageName() async {
-  var pubSpecFile = File("./pubspec.yaml");
-  var content = await pubSpecFile.readAsString(
-    encoding: utf8,
-  );
-
-  var lines =
-      content.contains("\r\n") ? content.split("\r\n") : content.split("\n");
-
-  var selectedLines =
-      lines.where((line) => line.startsWith("name: ")).toList()[0];
-
-  packageName = selectedLines.split("name: ")[1];
-  packageName = packageName.replaceAll("\r\n", "");
-  packageName = packageName.replaceAll("\n", "");
-  packageName = packageName.replaceAll(RegExp(r"/^\s\n+|\s\n+$/g"), "");
-  packageName = packageName;
-
-  packageName.split("\n")[0];
 }
 
 class CommonPackageExporter {
