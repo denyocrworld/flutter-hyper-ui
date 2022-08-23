071708dca5245a683afff56bb72c98d66e429f4c
Deny <denyocr.world@gmail.com>
2022-08-23 20:09:41 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
-        import 'package:flutter/material.dart';
-        import 'package:flutter_hyper_ui/core.dart';
-        import 'package:flutter_hyper_ui/setup.dart';
-        import 'package:get/get.dart';
-        
-        void main() async {
-          await initialize();
-        
-          Widget mainView = Container();
-          if (FirebaseAuth.instance.currentUser != null) {
-            mainView = Container();
-          }
-        
-          runApp(
-            GetMaterialApp(
-              debugShowCheckedModeBanner: false,
-              theme: defaultTheme,
-              home: mainView,
-            ),
-          );
-        }
+import 'package:flutter/material.dart';
+import 'package:flutter_hyper_ui/core.dart';
+import 'package:flutter_hyper_ui/setup.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  await initialize();
+
+  Widget mainView = Container();
+  if (FirebaseAuth.instance.currentUser != null) {
+    mainView = Container();
+  }
+
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: defaultTheme,
+      home: mainView,
+    ),
+  );
+}
